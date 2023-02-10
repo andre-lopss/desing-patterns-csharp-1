9ab33f98b88ff7d5995c26165e1902c67346aaf7
André Lopes <andrelneres@hotmail.com>
2023-02-10 10:08:29 -0300

    Padrão Strategy

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IImposto iss = new ISS();
+            IImposto icms = new ICMS();
+            IImposto iccc = new ICCC();
+
+            Orcamento orcamento = new Orcamento(3000.0);
+
+            CalculadorDeImpostos calculador = new CalculadorDeImpostos();
+            //calculador.RealizarCalculo(orcamento, iss);
+            //calculador.RealizarCalculo(orcamento, icms);
+            calculador.RealizarCalculo(orcamento, iccc);
+
+            Console.ReadKey();
+        }
+    }
+}
