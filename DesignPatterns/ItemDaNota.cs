5e0521eeb7105fdcbb9c20e363a9c9cef887177b
André Lopes <andrelneres@hotmail.com>
2023-02-14 11:48:20 -0300

    Builder

---
+++ DesignPatterns/ItemDaNota.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public class ItemDaNota
+    {
+        public ItemDaNota(string nome, double valor)
+        {
+            Nome = nome;
+            Valor = valor;
+        }
+
+        public string Nome { get; private set; }
+        public double Valor { get; private set; }
+
+
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -10,18 +10,18 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            Orcamento reforma = new Orcamento(500);
+            NotaFiscal nf = new NotaFiscalBuilder()
+                .ParaEmpresa("Teste e testa")
+                .ComCnpj("20.000.000/0001-20")
+                .Com(new ItemDaNota("item 1", 100.0))
+                .Com(new ItemDaNota("item 2", 200.0))
+                .NaDataAtual()
+                .ComObservacoes("Testando uma observação na NF'e")
+                .Constroi();
 
-            reforma.AplicaDescontoExtra();
-            Console.WriteLine(reforma.Valor);
-            reforma.Aprova();
-
-            reforma.AplicaDescontoExtra();
-            Console.WriteLine(reforma.Valor);
-
-            reforma.Finaliza();
-
-            reforma.AplicaDescontoExtra();
+            Console.WriteLine(nf.ValorBruto);
+            Console.WriteLine(nf.Impostos);
+            Console.WriteLine(nf.DataDeEmissao);
             Console.ReadKey();
         }
     }
